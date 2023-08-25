83f62322c4126b46910612405c5c4dfbce7a183a
samstickkz <samstickkz@gmail.com>
2023-08-25 23:33:02 +0100

    fixes

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import cards from "../../assets/cards.png";
 import "./homepage.css";
-
+import { GrStatusGood } from "react-icons/gr";
+import { AiOutlineArrowRight } from "react-icons/ai";
 const Homepage = () => {
   return (
     <section id="homepage">
@@ -16,15 +17,37 @@ const Homepage = () => {
             <br /> sed do eiusmod tempor incididunt ut labore.
           </p>
 
+          <div className="items_list">
+            <div>
+              <GrStatusGood className="icons_color" />
+              <p>Instant Transfer</p>
+            </div>
+            <div>
+              <GrStatusGood className="icons_col" />
+              <p>Instant Transfer</p>
+            </div>{" "}
+            <div>
+              <GrStatusGood className="icons_clors" />
+
+              <p>Instant Transfer</p>
+            </div>{" "}
+            <div>
+              <GrStatusGood className="icons_colors" />
+              <p>Instant Transfer</p>
+            </div>
+          </div>
+
           <div className="lower_btn">
             <button className="btn">Open Account</button>
             <div>
-              <a href="#" className="btn-primary">
+              <a href="#" className="btn-primary comp">
                 Compare Prices
               </a>
+              <AiOutlineArrowRight className="icons_color" />
             </div>
           </div>
         </div>
+
         <div className="home_right">
           <img src={cards} alt="" />
         </div>
