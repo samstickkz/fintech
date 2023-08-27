822462d376d2fa39c6d99febcff32a4c30f7cd71
samstickkz <samstickkz@gmail.com>
2023-08-27 13:23:57 +0100

    fixes

--- src/components/saving/saving.jsx
+++ src/components/saving/saving.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./saving.css";
-import { GiLightningFrequency } from "react-icons/gi";
+import airplane from "../../assets/airplane.png";
+import bicycle from "../../assets/bicycle.png";
+import camera from "../../assets/camera.png";
+import laptop from "../../assets/laptop.png";
+import addbutton from "../../assets/addbutton.png";
 
 const Savings = () => {
   return (
@@ -26,35 +30,45 @@ const Savings = () => {
         <div className="saving-bottom">
           <div>
             <div className="box-blue blue">
-              <GiLightningFrequency className="icon-h" />
+              <div className="airplane">
+                <img src={airplane} />
+              </div>
             </div>
             <h5>new Laptop</h5>
             <small>$400</small>
           </div>
           <div>
             <div className="box-blue pink">
-              <GiLightningFrequency className="icon-h" />
+              <div className="airplane">
+                <img src={bicycle} />
+              </div>
             </div>
             <h5>new Laptop</h5>
             <small>$400</small>
           </div>{" "}
           <div>
             <div className="box-blue bluer">
-              <GiLightningFrequency className="icon-h" />
+              <div className="airplane">
+                <img src={camera} />
+              </div>
             </div>
             <h5>new Laptop</h5>
             <small>$400</small>
           </div>{" "}
           <div>
             <div className="box-blue ash ">
-              <GiLightningFrequency className="icon-h" />
+              <div className="airplane">
+                <img src={laptop} />
+              </div>
             </div>
             <h5>new Laptop</h5>
             <small>$400</small>
           </div>{" "}
           <div>
             <div className="box-blue fade">
-              <GiLightningFrequency className="icon-h" />
+              <div className="airplane">
+                <img src={addbutton} />
+              </div>
             </div>
           </div>
         </div>
