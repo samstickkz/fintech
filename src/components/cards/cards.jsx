e61443754eb1356277b4064ee83f1a108e3d631a
samstickkz <samstickkz@gmail.com>
2023-08-27 16:42:19 +0100

    cards

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -4,26 +4,28 @@ import cards from "../../assets/cards.png";
 
 const Cards = () => {
   return (
-    <div className="center-container">
-      <div className="container card">
-        <small>Account</small>
+    <section>
+      <div className="center-container">
+        <div className="container card">
+          <small>Account</small>
 
-        <h2>
-          Perfect card <br /> for your needs.
-        </h2>
-        <p>
-          Senectus et netus et malesuada fames ac turpis. <br /> Sagittis vitae
-          et leo duis ut diam.
-        </p>
-        <div className="card-img">
-          <img src={cards} alt="Card" />
-        </div>
-        <div className="open-btn">
-          <button className="btn one">Open Account</button>
-          <button className="btn_primary">Open Account</button>
+          <h2>
+            Perfect card <br /> for your needs.
+          </h2>
+          <p>
+            Senectus et netus et malesuada fames ac turpis. <br /> Sagittis
+            vitae et leo duis ut diam.
+          </p>
+          <div className="card-img">
+            <img src={cards} alt="Card" />
+          </div>
+          <div className="open-btn">
+            <button className="btn one">Open Account</button>
+            <button className="btn_primary">Compare Cards</button>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
